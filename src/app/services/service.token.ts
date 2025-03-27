b5f1df7442289e4398b3b671cd67158ab7890045
Amanda <amanda.profissional078@gmail.com>
2025-03-26 22:36:10 -0300

    add snackbar

--- src/app/services/service.token.ts
+++ src/app/services/service.token.ts
@@ -1,10 +1,12 @@
 import { InjectionToken } from "@angular/core";
 import { ICLientService } from "./api-client/clients/iclients.service";
+import { ISnackBarManagerService } from "./isnackbar-manager.service";
 
 export const SERVICES_TOKEN = {
     HTTP:{
         CLIENT: new InjectionToken<ICLientService>('SERVICES_TOKEN.HTTP.CLIENT'),
        // SCHEDULE: new InjectionToken<IScheduleService>('SERVICES_TOKEN.HTTP.SCHEDULE')
 
-    }
+    }, 
+    SNACKBAR: new InjectionToken<ISnackBarManagerService>('SERVICES_TOKEN.SNACKBAR')
 }
